1f79c59f233fea2983f1e1a6392bd6a55c267c19
achmadrizky486 <achmadrizky486@gmail.com>
2022-03-23 15:53:51 +0700

    Add TodoList

---
+++ todo/src/todo-list/TodoList.js
@@ -0,0 +1,14 @@
+import "./TodoList.css"
+
+const TodoList = () => {
+    return (
+        <ul>
+            <li>Makan bang</li>
+            <li>Mancing</li>
+            <li>Memecahkan pala bapak kau</li>
+        </ul>
+    )
+
+}
+
+export default TodoList
