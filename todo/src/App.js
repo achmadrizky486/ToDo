798ffa1ecc9956b6bad92a9738081e40de82655a
achmadrizky486 <achmadrizky486@gmail.com>
2022-03-23 15:53:25 +0700

    Update app

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Todo from './todo/Todo';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Todo />
     </div>
-  );
+  )
 }
+
+
+// -----Buat Component didalam-------------------------------
+// class Header extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <h2>Ini Component</h2>
+//       </div>
+//     )
+//   }
+// }
+// ---------------------------------------------------------
+
+// -----------Render Pakai Function-------------------------
+// function App() {
+//   return (
+//     <div>
+//       <h1 className='header'>
+//         Hello World
+//       </h1>
+//       <Header />
+//       <p>Lorem ipsum dolor sit amet</p>
+//     </div>
+//   )
+// }
+// -----------------------------------------------------
+
+
+// -----------------Render pakai class---------------------
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1 className='header'>
+//           Hello World
+//         </h1>
+//         <p>Lorem ipsum dolor sit amet</p>
+//       </div>
+//     )
+//   }
+// }
+// -----------------------------------------------------
 
 export default App;
